80337b2bb235adc95368d809b46e24ee14c3481d
AgentKloon <87957190+Dctrrr@users.noreply.github.com>
2022-09-04 18:43:00 +0530

    Add files via upload

---
+++ src/main/java/spacehelm/dctrspacehelm/DctrConfigListener.java
@@ -0,0 +1,19 @@
+package spacehelm.dctrspacehelm;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DctrConfigListener extends JavaPlugin implements Listener {
+
+    FileConfiguration config = getConfig();
+
+    @Override
+    public void onEnable() {
+        config.addDefault("config.yml", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+}
