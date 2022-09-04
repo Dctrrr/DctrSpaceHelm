f69265e76ca20a07d1b8889308e2fb3e58abd12c
Dctrrr <first.tab.lenovo90@gmail.com>
2022-09-04 22:30:58 +0530

    just a quick fix

---
+++ DctrSpaceHelm-master/src/main/java/spacehelm/dctrspacehelm/DctrSpaceHelm.java
@@ -0,0 +1,96 @@
+package spacehelm.dctrspacehelm;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class DctrSpaceHelm extends JavaPlugin {
+    
+    private static DctrSpaceHelm plugin;
+    private int editionNUmber;
+
+    public void setEditionNumber(int newEdition) {
+        this.editionNumber = newEdition;
+    }
+
+    public int getEditionNumber() {
+        return this.editionNumber;
+    }
+
+    FileConfiguration config = getConfig();
+    int editionNumber;
+    
+    public static JavaPlugin getInstance() {
+        return plugin;
+    }
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        
+        config.addDefault("editionNumber", 1);
+        config.options().copyDefaults(true);
+        this.saveConfig();
+        editionNumber = config.getInt("editionNumber");
+
+        getCommand("spacehelm").setExecutor(new DctrSpaceHelmCMD());
+        getServer().getPluginManager().registerEvents(new DctrSpaceHelmListener(), this);
+
+        List<Material> colors = new ArrayList<>();
+        colors.add(Material.RED_STAINED_GLASS);
+        colors.add(Material.BLACK_STAINED_GLASS);
+        colors.add(Material.BLUE_STAINED_GLASS);
+        colors.add(Material.BROWN_STAINED_GLASS);
+        colors.add(Material.CYAN_STAINED_GLASS);
+        colors.add(Material.GREEN_STAINED_GLASS);
+        colors.add(Material.LIGHT_BLUE_STAINED_GLASS);
+        colors.add(Material.LIME_STAINED_GLASS);
+        colors.add(Material.LIGHT_GRAY_STAINED_GLASS);
+        colors.add(Material.MAGENTA_STAINED_GLASS);
+        colors.add(Material.ORANGE_STAINED_GLASS);
+        colors.add(Material.YELLOW_STAINED_GLASS);
+        colors.add(Material.PINK_STAINED_GLASS);
+        colors.add(Material.PURPLE_STAINED_GLASS);
+        colors.add(Material.GRAY_STAINED_GLASS);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+
+                    ItemStack item = player.getInventory().getHelmet();
+
+                    if(item == null) {return;}
+                    if(item.getItemMeta() == null) {return;}
+                    if(item.getItemMeta().getDisplayName() == null) {return;}
+                    ItemMeta meta = item.getItemMeta();
+                    String ItemName = item.getItemMeta().getDisplayName();
+                    if (ItemName.equals(ChatColor.RED + "Dctr's Space Helmet")) {
+                        Random r = new Random();
+                        int color = r.nextInt(colors.size());
+                        Material material = colors.get(color);
+                        item.setType(material);
+                        item.setItemMeta(meta);
+                    }
+                }
+            }
+        }.runTaskTimer(this, 20, 20);
+    }
+    
+    @Override
+    public void onDisable() {
+        config.set("editionNumber", editionNumber);
+        saveConfig();
+    }
+}
---
+++ DctrSpaceHelm-master/src/main/java/spacehelm/dctrspacehelm/DctrSpaceHelmCMD.java
@@ -0,0 +1,69 @@
+package spacehelm.dctrspacehelm;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DctrSpaceHelmCMD implements CommandExecutor {
+    
+    JavaPlugin plugin = DctrSpaceHelm.getPlugin(DctrSpaceHelm.class);
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (args.length >= 1) {
+            String playerName = args[0];
+            Player targetPlayer = Bukkit.getPlayer(playerName);
+        }
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (args.length >= 1) {
+                String playerName = args[0];
+                player = Bukkit.getPlayer(playerName);
+            }
+
+            assert player != null;
+            if (player.hasPermission("adminitem.spacehelm")) {
+                ItemStack itemStack = new ItemStack(Material.RED_STAINED_GLASS);
+
+                ItemMeta itemMeta = itemStack.getItemMeta();
+                itemMeta.setDisplayName(ChatColor.RED + "Dctr's Space Helmet");
+
+                itemMeta.setLore(Arrays.asList(
+                        ChatColor.GRAY + "" + ChatColor.ITALIC + "A rare space helmet forged",
+                        ChatColor.GRAY + "" + ChatColor.ITALIC + "from shards of moon.",
+                        " ",
+                        ChatColor.DARK_GRAY + "To: " + player.getDisplayName(),
+                        ChatColor.DARK_GRAY + "From: " + ChatColor.RED + "[ADMIN] " + ((Player)sender).getDisplayName(),
+                        " ",
+                        ChatColor.DARK_GRAY + "Edition: #" + (plugin.getConfig()),
+                        " ",
+                        ChatColor.RED + "" + ChatColor.BOLD + "SPECIAL"
+
+                ));
+
+                itemStack.setItemMeta(itemMeta);
+                player.getInventory().setHelmet(new ItemStack(itemStack));
+                itemMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+                player.sendMessage(((Player)sender).getName() + "§egave you a§e §cDctr's Space Helmet§c (§e001)§e!");
+            } else {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cYou don't have access to this command!"));
+            }
+        }
+        return true;
+    }
+}
+
